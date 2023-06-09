163dd9068fbc58ccad343891f9dcadd00f9dc866
Andrey Rebrov <aandreyrebrov@gmail.com>
2023-06-09 22:23:02 +0300

    refactored PlayerComponent gitignore updated

--- Code/Components/Player.h
+++ Code/Components/Player.h
@@ -1,174 +1,63 @@
 // Copyright 2017-2019 Crytek GmbH / Crytek Group. All rights reserved.
 #pragma once
 
-#include <array>
-#include <numeric>
-
-#include <CryEntitySystem/IEntityComponent.h>
-#include <CryMath/Cry_Camera.h>
-
-#include <ICryMannequin.h>
-#include <CrySchematyc/Utils/EnumFlags.h>
-
 #include <DefaultComponents/Cameras/CameraComponent.h>
+#include <DefaultComponents/Input/InputComponent.h>
 #include <DefaultComponents/Physics/CharacterControllerComponent.h>
 #include <DefaultComponents/Geometry/AdvancedAnimationComponent.h>
-#include <DefaultComponents/Input/InputComponent.h>
-#include <DefaultComponents/Audio/ListenerComponent.h>
 
 ////////////////////////////////////////////////////////
 // Represents a player participating in gameplay
 ////////////////////////////////////////////////////////
-class CPlayerComponent final : public IEntityComponent
+namespace Game
 {
-	enum class EInputFlagType
+	class CPlayerComponent final : public IEntityComponent
 	{
-		Hold = 0,
-		Toggle
-	};
-
-	enum class EInputFlag : uint8
-	{
-		MoveLeft = 1 << 0,
-		MoveRight = 1 << 1,
-		MoveForward = 1 << 2,
-		MoveBack = 1 << 3
-	};
-	
-	static constexpr EEntityAspects InputAspect = eEA_GameClientD;
-
-	template<typename T, size_t SAMPLES_COUNT>
-	class MovingAverage
-	{
-		static_assert(SAMPLES_COUNT > 0, "SAMPLES_COUNT shall be larger than zero!");
-
 	public:
+		CPlayerComponent() = default;
+		virtual ~CPlayerComponent() = default;
 
-		MovingAverage()
-			: m_values()
-			, m_cursor(SAMPLES_COUNT)
-			, m_accumulator()
+		virtual void Initialize() override;
+
+		virtual Cry::Entity::EventFlags GetEventMask() const override;
+		virtual void ProcessEvent(const SEntityEvent& event) override;
+
+		// Reflect type to set a unique identifier for this component
+		static void ReflectType(Schematyc::CTypeDesc<CPlayerComponent>& desc)
 		{
+			desc.SetGUID("{63F4C0C6-32AF-4ACB-8FB0-57D45DD14725}"_cry_guid);
+			desc.AddMember(&CPlayerComponent::m_movementSpeed, 'pms', "playermovespeed", "Player Movement Speed", "Sets the Player MS", ZERO);
+			desc.AddMember(&CPlayerComponent::m_rotationSpeed, 'prs', "playerrotationspeed", "Player Rotation Speed", "Sets the speed of the players rotationPlayer", ZERO);
+			desc.AddMember(&CPlayerComponent::m_cameraDefaultPos, 'cdp', "cameradefaultpos", "Camera Default Position", "Sets the camera position", ZERO);
+			desc.AddMember(&CPlayerComponent::m_rotationLimitsMinPitch, 'cpmi', "camerapitchmin", "Camera Pitch Min", "Minimum rotation value for camera pitch", -0.85f);
+			desc.AddMember(&CPlayerComponent::m_rotationLimitsMaxPitch, 'cpm', "camerapitchmax", "Camera Pitch Max", "Maximum rotation value for camera pitch", 1.5f);
 		}
 
-		MovingAverage& Push(const T& value)
-		{
-			if (m_cursor == SAMPLES_COUNT)
-			{
-				m_values.fill(value);
-				m_cursor = 0;
-				m_accumulator = std::accumulate(m_values.begin(), m_values.end(), T(0));
-			}
-			else
-			{
-				m_accumulator -= m_values[m_cursor];
-				m_values[m_cursor] = value;
-				m_accumulator += m_values[m_cursor];
-				m_cursor = (m_cursor + 1) % SAMPLES_COUNT;
-			}
-
-			return *this;
-		}
-
-		T Get() const
-		{
-			return m_accumulator / T(SAMPLES_COUNT);
-		}
-
-		void Reset()
-		{
-			m_cursor = SAMPLES_COUNT;
-		}
+	protected:
 
 	private:
+		void InitializeInput();
+		void UpdatePlayerMovement();
+		void UpdateCameraRotation();
 
-		std::array<T, SAMPLES_COUNT> m_values;
-		size_t m_cursor;
+	private:
+		Cry::DefaultComponents::CCameraComponent* m_pCameraComponent = nullptr;
+		Cry::DefaultComponents::CInputComponent* m_pInputComponent = nullptr;
+		Cry::DefaultComponents::CCharacterControllerComponent* m_pCharacterControllerComponnet = nullptr;
+		Cry::DefaultComponents::CAdvancedAnimationComponent* m_pAdvancedAnimationComponent = nullptr;
 
-		T m_accumulator;
+		Quat m_lookOrientation;
+
+		Vec3 m_cameraDefaultPos;
+
+		Vec2 m_movementDelta;
+		Vec2 m_mouseDeltaRotation;
+
+		float m_movementSpeed = 0.0f;
+		float m_rotationSpeed = 0.0f;
+
+		float m_rotationLimitsMinPitch = 0.0f;
+		float m_rotationLimitsMaxPitch = 0.0f;
 	};
+}
 
-public:
-	CPlayerComponent() = default;
-	virtual ~CPlayerComponent() = default;
-
-	// IEntityComponent
-	virtual void Initialize() override;
-
-	virtual Cry::Entity::EventFlags GetEventMask() const override;
-	virtual void ProcessEvent(const SEntityEvent& event) override;
-	
-	virtual bool NetSerialize(TSerialize ser, EEntityAspects aspect, uint8 profile, int flags) override;
-	virtual NetworkAspectType GetNetSerializeAspectMask() const override { return InputAspect; }
-	// ~IEntityComponent
-
-	// Reflect type to set a unique identifier for this component
-	static void ReflectType(Schematyc::CTypeDesc<CPlayerComponent>& desc)
-	{
-		desc.SetGUID("{63F4C0C6-32AF-4ACB-8FB0-57D45DD14725}"_cry_guid);
-	}
-
-	void OnReadyForGameplayOnServer();
-	bool IsLocalClient() const { return (m_pEntity->GetFlags() & ENTITY_FLAG_LOCAL_PLAYER) != 0; }
-
-protected:
-	void Revive(const Matrix34& transform);
-
-	void UpdateMovementRequest(float frameTime);
-	void UpdateLookDirectionRequest(float frameTime);
-	void UpdateAnimation(float frameTime);
-	void UpdateCamera(float frameTime);
-
-	void HandleInputFlagChange(CEnumFlags<EInputFlag> flags, CEnumFlags<EActionActivationMode> activationMode, EInputFlagType type = EInputFlagType::Hold);
-
-	// Called when this entity becomes the local player, to create client specific setup such as the Camera
-	void InitializeLocalPlayer();
-	
-	// Start remote method declarations
-protected:
-	// Parameters to be passed to the RemoteReviveOnClient function
-	struct RemoteReviveParams
-	{
-		// Called once on the server to serialize data to the other clients
-		// Then called once on the other side to deserialize
-		void SerializeWith(TSerialize ser)
-		{
-			// Serialize the position with the 'wrld' compression policy
-			ser.Value("pos", position, 'wrld');
-			// Serialize the rotation with the 'ori0' compression policy
-			ser.Value("rot", rotation, 'ori0');
-		}
-		
-		Vec3 position;
-		Quat rotation;
-	};
-	// Remote method intended to be called on all remote clients when a player spawns on the server
-	bool RemoteReviveOnClient(RemoteReviveParams&& params, INetChannel* pNetChannel);
-	
-protected:
-	bool m_isAlive = false;
-
-	Cry::DefaultComponents::CCameraComponent* m_pCameraComponent = nullptr;
-	Cry::DefaultComponents::CCharacterControllerComponent* m_pCharacterController = nullptr;
-	Cry::DefaultComponents::CAdvancedAnimationComponent* m_pAnimationComponent = nullptr;
-	Cry::DefaultComponents::CInputComponent* m_pInputComponent = nullptr;
-	Cry::Audio::DefaultComponents::CListenerComponent* m_pAudioListenerComponent = nullptr;
-
-	FragmentID m_idleFragmentId;
-	FragmentID m_walkFragmentId;
-	TagID m_rotateTagId;
-
-	CEnumFlags<EInputFlag> m_inputFlags;
-	Vec2 m_mouseDeltaRotation;
-	MovingAverage<Vec2, 10> m_mouseDeltaSmoothingFilter;
-
-	const float m_rotationSpeed = 0.002f;
-
-	int m_cameraJointId = -1;
-
-	FragmentID m_activeFragmentId;
-
-	Quat m_lookOrientation; //!< Should translate to head orientation in the future
-	float m_horizontalAngularVelocity;
-	MovingAverage<float, 10> m_averagedHorizontalAngularVelocity;
-};
--- Code/GamePlugin.cpp
+++ Code/GamePlugin.cpp
@@ -2,8 +2,6 @@
 #include "StdAfx.h"
 #include "GamePlugin.h"
 
-#include "Components/Player.h"
-
 #include <CrySchematyc/Env/IEnvRegistry.h>
 #include <CrySchematyc/Env/EnvPackage.h>
 #include <CrySchematyc/Utils/SharedString.h>
@@ -16,11 +14,6 @@
 
 CGamePlugin::~CGamePlugin()
 {
-	// Remove any registered listeners before 'this' becomes invalid
-	if (gEnv->pGameFramework != nullptr)
-	{
-		gEnv->pGameFramework->RemoveNetworkedClientListener(*this);
-	}
 
 	gEnv->pSystem->GetISystemEventDispatcher()->RemoveListener(this);
 
@@ -45,9 +38,6 @@ void CGamePlugin::OnSystemEvent(ESystemEvent event, UINT_PTR wparam, UINT_PTR lp
 		// Called when the game framework has initialized and we are ready for game logic to start
 		case ESYSTEM_EVENT_GAME_POST_INIT:
 		{
-			// Listen for client connection events, in order to create the local player
-			gEnv->pGameFramework->AddNetworkedClientListener(*this);
-
 			// Don't need to load the map in editor
 			if (!gEnv->IsEditor())
 			{
@@ -83,89 +73,8 @@ void CGamePlugin::OnSystemEvent(ESystemEvent event, UINT_PTR wparam, UINT_PTR lp
 		
 		case ESYSTEM_EVENT_LEVEL_UNLOAD:
 		{
-			m_players.clear();
 		}
 		break;
-	}
-}
-
-bool CGamePlugin::OnClientConnectionReceived(int channelId, bool bIsReset)
-{
-	// Connection received from a client, create a player entity and component
-	SEntitySpawnParams spawnParams;
-	spawnParams.pClass = gEnv->pEntitySystem->GetClassRegistry()->GetDefaultClass();
-	
-	// Set a unique name for the player entity
-	const string playerName = string().Format("Player%" PRISIZE_T, m_players.size());
-	spawnParams.sName = playerName;
-	
-	// Set local player details
-	if (m_players.empty() && !gEnv->IsDedicated())
-	{
-		spawnParams.id = LOCAL_PLAYER_ENTITY_ID;
-		spawnParams.nFlags |= ENTITY_FLAG_LOCAL_PLAYER;
-	}
-
-	// Spawn the player entity
-	if (IEntity* pPlayerEntity = gEnv->pEntitySystem->SpawnEntity(spawnParams))
-	{
-		// Set the local player entity channel id, and bind it to the network so that it can support Multiplayer contexts
-		pPlayerEntity->GetNetEntity()->SetChannelId(channelId);
-
-		// Create the player component instance
-		CPlayerComponent* pPlayer = pPlayerEntity->GetOrCreateComponentClass<CPlayerComponent>();
-
-		if (pPlayer != nullptr)
-		{
-			// Push the component into our map, with the channel id as the key
-			m_players.emplace(std::make_pair(channelId, pPlayerEntity->GetId()));
-		}
-	}
-
-	return true;
-}
-
-bool CGamePlugin::OnClientReadyForGameplay(int channelId, bool bIsReset)
-{
-	// Revive players when the network reports that the client is connected and ready for gameplay
-	auto it = m_players.find(channelId);
-	if (it != m_players.end())
-	{
-		if (IEntity* pPlayerEntity = gEnv->pEntitySystem->GetEntity(it->second))
-		{
-			if (CPlayerComponent* pPlayer = pPlayerEntity->GetComponent<CPlayerComponent>())
-			{
-				pPlayer->OnReadyForGameplayOnServer();
-			}
-		}
-	}
-
-	return true;
-}
-
-void CGamePlugin::OnClientDisconnected(int channelId, EDisconnectionCause cause, const char* description, bool bKeepClient)
-{
-	// Client disconnected, remove the entity and from map
-	auto it = m_players.find(channelId);
-	if (it != m_players.end())
-	{
-		gEnv->pEntitySystem->RemoveEntity(it->second);
-
-		m_players.erase(it);
-	}
-}
-
-void CGamePlugin::IterateOverPlayers(std::function<void(CPlayerComponent& player)> func) const
-{
-	for (const std::pair<int, EntityId>& playerPair : m_players)
-	{
-		if (IEntity* pPlayerEntity = gEnv->pEntitySystem->GetEntity(playerPair.second))
-		{
-			if (CPlayerComponent* pPlayer = pPlayerEntity->GetComponent<CPlayerComponent>())
-			{
-				func(*pPlayer);
-			}
-		}
 	}
 }
 
